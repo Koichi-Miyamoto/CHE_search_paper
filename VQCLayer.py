0009bd22f4c87be2edd3a1b30d5c6d394d1e9f59
miyamone <monmon38mon@gmail.com>
2021-12-01 09:39:10 +0900

    First trial of ResNet+VQC (using PennyLane, ResNet w/o the last layer + (2048->4) Dense layer + (nQubit=4,depth=4) VQC)

---
+++ VQCLayer.py
@@ -0,0 +1,27 @@
+# make a VQC as a TensorFlow layer by PennyLane
+
+import numpy
+import tensorflow as tf
+import pennylane as qml
+pi = numpy.pi
+
+def makeVQCLayer(nQubit, depth, outputQubitsId = [0]):
+    
+    dev = qml.device("default.qubit", wires=nQubit)
+    
+    def makeCircuit(inputs, weights):
+        # inputs must by in [-1,1]
+        qml.AngleEmbedding(inputs * 0.5 * pi, wires = range(nQubit), rotation='Y')
+        qml.BasicEntanglerLayers(weights, wires=range(nQubit), rotation=qml.RY)
+        return [qml.expval(qml.PauliZ(wires=i)) for i in outputQubitsId]
+    
+    return qml.qnn.KerasLayer(qml.QNode(makeCircuit, dev),
+                               {"weights": (depth, nQubit)},
+                               output_dim=len(outputQubitsId))
+
+# def makeCircuit(inputs, weights, nQubit, outputQubitsId):
+#     # inputs must by in [-1,1]
+#     qml.AngleEmbedding(tf.math.asin(inputs), wires = range(nQubit), rotation='Y')
+#     qml.BasicEntanglerLayers(weights, wires=range(nQubit), rotation=qml.RY)
+#     return [qml.expval(qml.PauliZ(wires=i)) for i in outputQubitsId]
+    
